8a60dc11ad0a6a7c31a33fdb8217a5e6cfb9e94d
Yelizaveta Lohvynovska <elizave8@gmail.com>
2025-03-09 20:05:43 +0200

    made 2-8 tasks

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -22,3 +22,153 @@
 //   }
 // }
 // logItems(styles);
+
+// Напишіть функцію checkLogin(array), яка:
+// Приймає масив логінів як аргумент.
+// Запитує ім'я користувача через prompt.
+// Перевіряє, чи є введене ім'я у переданому масиві.
+// Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
+// Якщо ім'я відсутнє – виводить повідомлення: "User not found".
+
+// const logins = ['Peter', 'John', 'Igor', 'Sasha'];
+
+// function checkLogin(array) {
+//   const userName = prompt('What is your name?');
+//   if (array.includes(userName)) {
+//     alert(`Welcome ${userName}`);
+//   } else {
+//     alert('User not found');
+//   }
+// }
+// checkLogin(logins);
+
+// Напишіть функцію caclculateAverage(),
+// яка приймає довільну кількість
+// аргументів і повертає їхнє середнє значення.
+// Додайте перевірку, що аргументи - це числа.
+
+// function caclculateAverage(...args) {
+//   let total = 0;
+//   let index = 0;
+//   for (const arg of args) {
+//     if (typeof arg === 'number') {
+//       total += arg;
+//       index++;
+//     }
+//   }
+//   return Math.ceil(total / args.length);
+// }
+
+// console.log(caclculateAverage(11, 4, 7));
+// console.log(caclculateAverage(0, true, 'Liza'));
+// console.log(caclculateAverage(140, 0, 0));
+
+// Напишіть функцію, яка сумуватиме сусідні числа
+// і пушитиме їх в новий масив.
+
+//
+
+// уточнення: складати необхідно перше число з другим, потім друге - з третім,
+// третє - з четвертим і так до кінця.
+// В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// function toSum(array) {
+//   const newArr = [];
+//   for (let index = 0; index < array.length - 1; index++) {
+//     newArr.push(array[index] + array[index + 1]);
+//   }
+//   return newArr;
+// }
+// console.log(toSum(someArr));
+
+// Напишіть функцію findSmallestNumber(numbers),
+// яка шукає найменше число в масиві.
+// Додайте перевірку, що функція отримує саме масив, і
+// якщо функція отримує масив - поверніть з функції найменше число,
+// в іншому випадку - поверніть 'Sory, it is not an array!'.
+
+// const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
+// function findSmallestNumber(numbers) {
+//   if (Array.isArray(numbers)) {
+//     return Math.min(...numbers);
+//   } else {
+//     return 'Sorry, it is not an array!';
+//   }
+// }
+// console.log(findSmallestNumber(numbers));
+
+// Напишіть функцію findLongestWord(string), яка
+// приймає довільний рядок, що складається лише зі слів, розділених
+// пробілами (параметр string), і повертатиме найдовше слово у реченні.
+
+// Скористайтесь цим прикладом виклику функції для перевірки її роботи:
+// console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+// function findLongestWord(string) {
+//   const array = string.split(' ');
+//   let longest = '';
+//   for (const word of array) {
+//     if (word.length > longest.length) {
+//       longest = word;
+//     }
+//   }
+//   return longest;
+// }
+
+// console.log(findLongestWord('London is the capital of Great Britain')); // 'capital'
+
+// Напишіть скрипт, який для об'єкту user, послідовно:
+// 1 - додасть поле mood зі значенням 'happy',
+// 2 - замінить hobby на 'skydiving',
+// 3 - замінить значення premium на false,
+// 4 - виведе зміст об'єкта user у форматі
+// '<ключ>:<значення>' використовуя Object.keys() та for...of
+
+// const user = {
+//   name: 'John',
+//   age: 20,
+//   hobby: 'tenis',
+//   premium: true,
+// };
+
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
+
+// const keysOfObj = Object.keys(user);
+// const valuesOfObj = Object.values(user);
+// for (const key of keysOfObj) {
+//   console.log(`${key}:${user[key]}`);
+// }
+
+// Є об'єкт, в якому зберігаються зарплати команди
+// Напишіть код для додавання усіх зарплат та
+// збережіть його результат в змінній sum.
+// Якщо об'єкт salaries пустий, то результат має бути 0
+
+// const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
+
+// let sum = 0;
+// if (Object.keys(salaries).length === 0) {
+//   console.log(0);
+// }
+
+// for (const salarie of Object.values(salaries)) {
+//   sum += salarie;
+// }
+// console.log(sum);
+
+// Створіть об'єкт calculator з наступними методами:
+// read(a, b) - приймає два аргумента і зберігає їх як властивості об'єкта,
+// sum() - повертає сумму збереженних значень (з перевіркою на наявність властивостей в об'єкті),
+// mult() - перемножає збереженні значення і повертає результат (з перевіркою на наявність властивостей в об'єкті),
+// винесіть перевірку на наявність властивостей в об'єкті в окремий метод exist().
+
+// Якщо вказані властивості в обʼєкті відсутні (тобто метод exist повертає false),
+// методи sum і mult мають повертати рядок 'No such propeties'
